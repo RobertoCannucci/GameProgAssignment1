d11afa7b2ee6cb26469f1de9b95764b1f09c2419
Roberto Jr. Cannucci <2065436@edu.vaniercollege.qc.ca>
2023-09-21 11:03:51 -0400

    Basic Player Movement and Item movement

---
+++ My project/Assets/Scripts/BlueItemBehaviour.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueItemBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float timer = 0.0f;
+    public float maxTime = 0.2f;
+    float speed = 1.0f;
+    Vector3 movement = new Vector3(0, 0.1f, 0);
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        timer += Time.deltaTime;
+        if (timer > maxTime)
+        {
+            movement = -1.0f * movement;
+            timer = 0.0f;
+        }
+        transform.position = transform.position + movement * speed * Time.deltaTime;
+        transform.Rotate(0,0.5f,0*Time.deltaTime);
+    }
+}
